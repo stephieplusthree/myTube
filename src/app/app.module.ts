0599fe61f92ab8e0392cb9428c7039d749d6cd4b
github_username <you@domain.com>
2019-04-09 21:35:29 -0700

    main component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { VideoDetailComponent } from './main/video-detail/video-detail.component';
+import { VideoListComponent } from './main/video-list/video-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    VideoDetailComponent,
+    VideoListComponent
   ],
   imports: [
     BrowserModule,
